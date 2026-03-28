96b9a3ffb18d119bf1c8ebc78cbfcdf747f876e2
bafplus <bafplus@users.noreply.github.com>
2026-03-28 10:23:03 +0100

    Add Hank's Ratio training program

--- database/seeders/TrainingProgramSeeder.php
+++ database/seeders/TrainingProgramSeeder.php
@@ -14,6 +14,7 @@ class TrainingProgramSeeder extends Seeder
             ['name' => 'STR/DEF Build', 'str_percent' => 35, 'def_percent' => 35, 'spd_percent' => 15, 'dex_percent' => 15, 'is_custom' => false],
             ['name' => 'SPD/DEX Build', 'str_percent' => 15, 'def_percent' => 15, 'spd_percent' => 35, 'dex_percent' => 35, 'is_custom' => false],
             ['name' => 'Melee Special', 'str_percent' => 45, 'def_percent' => 25, 'spd_percent' => 15, 'dex_percent' => 15, 'is_custom' => false],
+            ['name' => "Hank's Ratio", 'str_percent' => 28, 'def_percent' => 35, 'spd_percent' => 28, 'dex_percent' => 10, 'is_custom' => false],
             ['name' => 'Custom', 'str_percent' => 25, 'def_percent' => 25, 'spd_percent' => 25, 'dex_percent' => 25, 'is_custom' => true],
         ];
 
