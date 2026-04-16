b05637ff0a470c5f05f5b9428977020fa65412f1
bart <bart.cockheyt@gmail.com>
2026-04-16 22:28:56 +0200

    Use database session instead of array

--- config/session.php
+++ config/session.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'driver' => env('SESSION_DRIVER', 'array'),
+    'driver' => env('SESSION_DRIVER', 'database'),
     'lifetime' => env('SESSION_LIFETIME', 120),
     'expire_on_close' => env('SESSION_EXPIRE_ON_CLOSE', false),
     'encrypt' => env('SESSION_ENCRYPT', false),
